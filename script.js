22630aee13ea4bcb1275a982380b21f1fefc8e3d
KolosRoman <romakolos84@gmail.com>
2023-02-25 16:04:44 +0200

    Add index.html and script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+let a = +prompt('input first operand');
+let b = +prompt('input second operand');
+
+console.log('Calculations are finished!');
+console.log('Sum: ' + a + ' + ' + b + ' = ' + (a + b));
+console.log('Diff: ' + a + ' - ' + b + ' = ' + (a - b));
+console.log('Mult: ' + a + ' * ' + b + ' = ' + (a * b));
+console.log('Div: ' + a + ' / ' + b + ' = ' + (a / b));
